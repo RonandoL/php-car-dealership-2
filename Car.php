5d189465d7b0a3b22a2742dfb393a93b0cfac02a
Ronando Long <ronando.long@gmail.com>
2016-02-08 22:27:36 -0800

    It will accept user input for dollar amount user is willing to spend, and will spit out results of autos that are less than the price submitted.

--- Car.php
+++ Car.php
@@ -9,17 +9,31 @@
 
         // Object Constructor: runs automatically when making Car object
           // an argument for each property, creates new cars with 1 line of code
-        function __construct($make_model, $price, $miles, $status="new", $image)
+        function __construct($make_model, $price, $miles, $image, $status="new")
         {
           $this->make_model = $make_model;
           $this->price = $price;
           $this->miles = $miles;
+          $this->image = $image;
           $this->status = $status;
-          $image->image = $image;
         }
     }
 
-    $car1 = new Car("Ford F150", 25000, 0, NULL, "images/f150.jpg")
+    $car1 = new Car("Ford F150", 25000, 0, "images/f150.jpg");
+    $car2 = new Car("Mercedes", 56500, 0, "images/mercedes.jpg");
+    $car3 = new Car("Lexus", 45000, 0, "images/lexus.jpg");
+    $car4 = new Car("Porsche 911", 115000, 0, "images/porsche.jpg");
+    $car5 = new Car("Ford", 32000, 55000, "images/ford.jpg", "used");
+    $car6 = new Car("Toyota Celica", 32500, 23450, "images/toyota.jpg", "used");
+
+    $cars = array($car1, $car2, $car3, $car4, $car5, $car6);
+
+    $cars_matching_search = array();
+    foreach ($cars as $car) {
+      if ($car->price < $_GET["price"]) {
+          array_push($cars_matching_search, $car);
+      }
+    }
 
 ?>
 
@@ -44,17 +58,21 @@
 
   <!-- Row: 1 -->
       <div class="row">
-        <div class="col-sm-3">
-
-  <!-- Form -->
-          <form action="Car.php">
-            <div class="form-group">
-              <label for="">Max price you want to pay:</label>
-              <input class="form-control" type="text" name="price" value="">
-            </div>
-
-            <button class="btn btn-info" type="submit" name="button">Gimme some sugar baby</button>
-          </form>
+        <div class="col-sm-6">
+            <h1>Your Car Dealership</h1>
+            <ul>
+              <?php
+                  foreach ($cars_matching_search as $car) {
+                    echo "<li> <img src='$car->image'></img>";
+                    echo "<li>" . $car->make_model . "</li>";
+                    echo "<ul>";
+                        echo "<li>$" . $car->price . "</li>";
+                        echo "<li> Miles: " . $car->miles . "</li>";
+                        echo "<li>" . $car->status . "</li>";
+                    echo "</ul>";
+                  }
+              ?>
+            </ul>
 
         </div>  <!-- /col-sm-12 -->
       </div>  <!-- /Row -->
